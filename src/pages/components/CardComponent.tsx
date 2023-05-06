ee20558195e04de6bf6561c21e5b47395595ce83
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-05-07 03:33:52 +0800

    data?

--- src/pages/components/CardComponent.tsx
+++ src/pages/components/CardComponent.tsx
@@ -5,30 +5,30 @@ const CardComponent = ({ data }: any) => {
                 <div className="flex items-center mb-5">
                     <p
                         className={`text-gray-900 ${
-                            data.aiRate >= 8.0 ? "bg-green-100" : "bg-red-100"
+                            data?.aiRate >= 8.0 ? "bg-green-100" : "bg-red-100"
                         } text-sm font-semibold inline-flex items-center p-1.5 rounded`}
                     >
-                        {data.aiRate}
+                        {data?.aiRate}
                     </p>
                     <p className="ml-2 font-medium text-gray-900 ">
-                        {data.aiRate >= 8.0 ? "Excellent" : "Unsuitable"}
+                        {data?.aiRate >= 8.0 ? "Excellent" : "Unsuitable"}
                     </p>
                 </div>
                 <div>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-blue-700 ">
-                        {data.company}
+                        {data?.company}
                     </h5>
                     <p className="mb-3 font-normal text-gray-700 ">
-                        {data.founder}
+                        {data?.founder}
                     </p>
                     <p className="mb-3 text-sm font-normal text-gray-700 ">
-                        {data.desc}
+                        {data?.desc}
                     </p>
                 </div>
 
                 <hr className="h-px my-6 bg-gray-200 border-0" />
 
-                {data.attr.map((item:any) => (
+                {data?.attr.map((item:any) => (
                     <div className="flex justify-between mt-4">
                         <span className="text-sm font-medium text-gray-900">
                             {item.type}
@@ -48,7 +48,7 @@ const CardComponent = ({ data }: any) => {
                 </div>
                 <div className="flow-root">
                     <ul role="list" className="flex justify-between">
-                        {data.similarComps.map((item:any) => (
+                        {data?.similarComps.map((item:any) => (
                             <li className="float-left py-3">
                                 <div className="flex items-center space-x-2">
                                     <div className="flex-shrink-0">
