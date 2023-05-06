be79271a714fcf679ab1309870478e5b9e98dae5
Syaamil Faiq <syaamil1136@gmail.com>
2023-05-06 18:44:58 +0800

    vet page

---
+++ src/pages/components/CardComponent.tsx
@@ -0,0 +1,70 @@
+const CardComponent = ({ data, similarComps }: any) => {
+    return (
+        <div className="bg-white border border-gray-200 rounded-lg shadow ">
+            <div className="p-5">
+                <div className="flex items-center mb-5">
+                    <p className="bg-green-100 text-green-800 text-sm font-semibold inline-flex items-center p-1.5 rounded">
+                        {data.aiRate}
+                    </p>
+                    <p className="ml-2 font-medium text-gray-900 ">
+                        {data.aiRateDesc}
+                    </p>
+                </div>
+                <div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-blue-700 ">
+                        {data.company}
+                    </h5>
+                    <p className="mb-3 font-normal text-gray-700 ">
+                        {data.founder}
+                    </p>
+                    <p className="mb-3 font-normal text-gray-700 ">
+                        {data.desc}
+                    </p>
+                </div>
+
+                <hr className="h-px my-6 bg-gray-200 border-0" />
+
+                <div className="flex justify-between mt-4">
+                    <span className="text-sm font-medium text-gray-900">
+                        Revenue
+                    </span>
+                    <span className="text-sm font-medium text-blue-700">
+                        RM6000000
+                    </span>
+                </div>
+
+                <hr className="h-px my-6 bg-gray-200 border-0" />
+
+                <div className="flex items-center justify-between mb-4">
+                    <h5 className="font-bold leading-none text-gray-900 text-md dark:text-white">
+                        Similar in the industry
+                    </h5>
+                </div>
+                <div className="flow-root">
+                    <ul role="list" className="flex justify-between">
+                        {similarComps.map((item) => (
+                            <li className="float-left py-3">
+                                <div className="flex items-center space-x-2">
+                                    <div className="flex-shrink-0">
+                                        <img
+                                            className="w-10 h-10 border-2 border-gray-300 rounded-full"
+                                            src={item.img}
+                                            alt="Neil image"
+                                        />
+                                    </div>
+                                    <div className="flex-1 min-w-0">
+                                        <p className="font-medium text-gray-900 truncate text-md dark:text-white">
+                                            {item.company}
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardComponent;
---
+++ src/pages/vet.tsx
@@ -0,0 +1,88 @@
+import CardComponent from "./components/CardComponent";
+const data = {
+    company: "Company Name",
+    founder: "Founder Name",
+    desc: "Description",
+    aiRate: "8.7",
+    aiRateDesc: "Excellent",
+};
+
+const similarComps = [
+    {
+        company: "Grab",
+        img: "/img/grab.png",
+    },
+    {
+        company: "Gojek",
+        img: "/img/gojek.jpg",
+    },
+    {
+        company: "Uber",
+        img: "/img/uber.png",
+    },
+];
+const vet = () => {
+    return (
+        <>
+            <div className="flex gap-8 columns-3">
+                <a
+                    href="#"
+                    className="container flex items-center justify-center bg-red-200 rounded-r-lg"
+                >
+                    <svg
+                        className="w-24 h-24 text-red-500"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                        ></path>
+                    </svg>
+                </a>
+                <div className="container">
+                    <CardComponent data={data} similarComps={similarComps} />
+                </div>
+                <a
+                    href="#"
+                    className="container flex items-center justify-center bg-green-200 rounded-l-lg"
+                >
+                    <svg
+                        className="w-24 h-24 text-green-500"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.5 12.75l6 6 9-13.5"
+                        ></path>
+                    </svg>
+                </a>
+            </div>
+
+            {/* <div className="container flex gap-10 py-8 mx-auto columns-3">
+                <div className="container">
+                    <CardComponent data={data} />
+                </div>
+                <div className="container">
+                    <CardComponent data={data} />
+                </div>
+                <div className="container">
+                    <CardComponent data={data} />
+                </div>
+            </div> */}
+        </>
+    );
+};
+
+export default vet;
