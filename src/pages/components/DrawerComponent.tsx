3edc54b995fa0befd1161cf38220f41c304cec1b
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-05-06 22:18:22 +0800

    Toast

--- src/pages/components/DrawerComponent.tsx
+++ src/pages/components/DrawerComponent.tsx
@@ -2,15 +2,31 @@ import React, { useEffect, useState } from "react";
 
 const DrawerComponent = ({ slideIn, handleSlideIn, data }: any) => {
   const [loading, setLoading] = useState(false);
+  const [toast, setToast] = useState(false);
   const handleLoading = () => {
     setLoading(true);
 
     setTimeout(() => {
       console.log("Hi");
       setLoading(false);
+      
       handleSlideIn();
+      setToast(true)
+      handleToast()
     }, 2000);
+
+
+    
   };
+  const handleToast = () =>{
+    setTimeout(() => {
+      console.log('Toast TIMEOUTE')
+      setToast(false)
+    }, 1000);
+  }
+  useEffect(()=>{
+
+  },[toast])
   return (
     <>
       <div
@@ -178,8 +194,9 @@ const DrawerComponent = ({ slideIn, handleSlideIn, data }: any) => {
 
       <div
         id="toast-success"
-        className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+        className="toast items-center w-full  max-w-xs p-4 mb-4 text-gray-800 bg-gray-50 rounded-lg shadow dark:text-gray-400 "
         role="alert"
+        style={{display:!toast?'none':'flex', bottom:'12px', left:'12px',position:'fixed'}}
       >
         <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
           <svg
@@ -200,7 +217,7 @@ const DrawerComponent = ({ slideIn, handleSlideIn, data }: any) => {
         <div className="ml-3 text-sm font-normal">Email sent successfully.</div>
         <button
           type="button"
-          className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+          className="ml-auto -mx-1.5 -my-1.5 bg-slate-200 text-gray-600 rounded-lg p-1.5  inline-flex h-8 w-8 "
           data-dismiss-target="#toast-success"
           aria-label="Close"
         >
