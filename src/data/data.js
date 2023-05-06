dc222d14a7ae4cc5a3f3ecede51da47ff0cc38e0
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-05-06 21:48:58 +0800

    Add data and loading indicator

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,133 @@
 const row = [
     {
         founder: "Alep",
-        company : 'Alif Group',
+        company: 'Alif Group',
         date: "30 July 2023",
         industry: "Finance",
-        description: "1000",
-        email : "",
-        contact : '',
-        location : ''
+        description: "A new emerging startup",
+        email: "aliff@gmail.com",
+        contact: '+601120935184',
+        location: 'Kuala Lumpur, Malaysia'
     },
     {
         founder: "Bar",
-        company : 'B Sdn Bhd',
+        company: 'B Sdn Bhd',
         date: "31 July 2023",
         industry: "Automotive",
-        description: "1000",
+        description: "A new emerging startup",
+        email: "barr@gmail.com",
+        contact: '+601120935184',
+        location: 'Pahang, Malaysia'
     },
+    {
+        founder: "Sarah",
+        company: 'Green Finance',
+        date: "15 June 2023",
+        industry: "Finance",
+        description: "A sustainable finance startup",
+        email: "sarah@greenfinance.com",
+        contact: '+60123456789',
+        location: 'Singapore'
+    },
+
+    {
+        founder: "John",
+        company: 'Fintech Innovations',
+        date: "1 August 2023",
+        industry: "Finance",
+        description: "Revolutionizing the fintech industry",
+        email: "john@fintechinnovations.com",
+        contact: '+60123456789',
+        location: 'Kelantan, Malaysia'
+    },
+
+    {
+        founder: "Jasmine",
+        company: 'Crypto Capital',
+        date: "10 September 2023",
+        industry: "Finance",
+        description: "Pioneering the world of cryptocurrency",
+        email: "jasmine@cryptocapital.com",
+        contact: '+447777777777',
+        location: 'London, UK'
+    }
+    ,
+    {
+        founder: "Hiro",
+        company: 'Quantum Finance',
+        date: "20 July 2023",
+        industry: "Finance",
+        description: "Using quantum computing to transform finance",
+        email: "hiro@quantumfinance.com",
+        contact: '+81333333333',
+        location: 'Tokyo, Japan'
+    }
+    ,
+    {
+        founder: "Maria",
+        company: 'Wealthfront',
+        date: "5 October 2023",
+        industry: "Finance",
+        description: "Making investing accessible to all",
+        email: "maria@wealthfront.com",
+        contact: '+14155555555',
+        location: 'San Francisco, USA'
+    }
+    ,
+    {
+        founder: "Ravi",
+        company: 'Smart Money',
+        date: "25 August 2023",
+        industry: "Finance",
+        description: "Empowering individuals to make smarter financial decisions",
+        email: "ravi@smartmoney.com",
+        contact: '+60123456789',
+        location: 'Terengganu, Malaysia'
+    }
+    ,
+    {
+        founder: "Sara",
+        company: 'MobilePay',
+        date: "15 July 2023",
+        industry: "Finance",
+        description: "The future of mobile payments",
+        email: "sara@mobilepay.com",
+        contact: '+46777777777',
+        location: 'Stockholm, Sweden'
+    }
+    ,
+    {
+        founder: "Diego",
+        company: 'Blockchain Capital',
+        date: "1 September 2023",
+        industry: "Finance",
+        description: "Investing in the future of blockchain",
+        email: "diego@blockchaincapital.com",
+        contact: '+60123456789',
+        location: 'Sabah, Malaysia'
+    },
+
+    {
+        founder: "Nadia",
+        company: 'Green Capital',
+        date: "10 November 2023",
+        industry: "Finance",
+        description: "Financing sustainable initiatives",
+        email: "nadia@greencapital.com",
+        contact: '+60123456789',
+        location: 'Selangor, Malaysia'
+    },
+
+    {
+        founder: "Sang",
+        company: 'AI Finance',
+        date: "20 October 2023",
+        industry: "Finance",
+        description: "Using AI to optimize financial services",
+        email: "sang@aifinance.com",
+        contact: '+60123456789',
+        location: 'Negeri Semblian, Malaysia'
+    }
 ];
 
 export default row;
